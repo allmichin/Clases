46d3ec33d490f2678f27c711c3b27ef4174b4314
Michelle Ceballos <dwight.uquq@gmail.com>
2025-08-26 22:23:44 -0600

    network1.py

--- network.py
+++ network.py
@@ -145,6 +145,9 @@ def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
+
+
+    #comentario random 2
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
 
